1c40bcddd1e037da10e42bb44e0125e6db571ef1
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-05-20 20:52:11 +0500

    upp

--- bank/banks/master.py
+++ bank/banks/master.py
@@ -10,22 +10,57 @@ bank_list = (
 )
 
 
-def get_all_data(daily):
+# def get_all_data(daily):
+#     from bank.models import Exchange as ex
+
+#     data = []
+
+#     for i in bank_list:
+#         temp_data = i().get_data()
+#         if temp_data["success"]:
+#             bank_name = temp_data['bank_name']
+#             olish = int(temp_data['olish'])
+
+#             sotish = int(temp_data['sotish'])
+#             ex.objects.create(
+#                 daily=daily,
+#                 bank_name=bank_name,
+#                 buy=olish, sell=sotish
+#             )
+#             data.append(temp_data)
+#     return data
+
+import threading
+
+
+def get_data_and_save(bank, daily):
     from bank.models import Exchange as ex
+    temp_data = bank().get_data()
+    if temp_data["success"]:
+        bank_name = temp_data['bank_name']
+        olish = int(temp_data['olish'])
+        sotish = int(temp_data['sotish'])
+        ex.objects.create(
+            daily=daily,
+            bank_name=bank_name,
+            buy=olish,
+            sell=sotish
+        )
+        return temp_data
+    return None
 
-    data = []
+def get_all_data(daily):
+    # bank_list = [Bank1, Bank2, Bank3]  # Bank1, Bank2, Bank3 gibi banka sınıflarını burada tanımlamanız gerekiyor
 
-    for i in bank_list:
-        temp_data = i().get_data()
-        if temp_data["success"]:
-            bank_name = temp_data['bank_name']
-            olish = int(temp_data['olish'])
+    threads = []
+    results = []
 
-            sotish = int(temp_data['sotish'])
-            ex.objects.create(
-                daily=daily,
-                bank_name=bank_name,
-                buy=olish, sell=sotish
-            )
-            data.append(temp_data)
-    return data
+    for bank in bank_list:
+        thread = threading.Thread(target=lambda: results.append(get_data_and_save(bank, daily)))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    return results
