1ca2723ef1de73e6181fa5b13563a61e6d72b257
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-05-20 21:10:08 +0500

    sa

--- bank/views.py
+++ bank/views.py
@@ -6,6 +6,7 @@ from .models import Exchange,Daily
 from .serializers import ExchangeSerializer
 from .banks.master import get_all_data
 from django.utils import timezone
+from django.core.cache import cache
 class Test(APIView):
     def get(self,request):
         data = get_all_data()
@@ -19,10 +20,24 @@ class ExchangeListAPIView(ListAPIView):
         if not obj or obj.date.date() < curr:
             obj = Daily.objects.create()
             obj.generate_daily_data()
-            
+        
+        
         return Exchange.objects.filter(daily_id=obj.id).order_by('bank_name')
 
+    def get(self,request,*args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
         
+        data = cache.get('currency_data')
+        if not data:
+            data = self.get_serializer(queryset, many=True).data
+            cache.set('my_cache_key', data, 60 * 150)
+        
+        return Response(data)
     serializer_class = ExchangeSerializer
     
     
