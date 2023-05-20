07b9446d7fbfb1df95f9ce2bdf4abc2248a74083
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-05-20 22:27:16 +0500

    'asdf'

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = BASE_DIR / 'static_files'
 MEDIA_URL = "/media/"
-# MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
