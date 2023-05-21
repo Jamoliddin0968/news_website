11c80f43bb7508ba004c46a01582923cf1b6eb8c
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-05-21 12:25:13 +0500

    asdf

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,8 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = BASE_DIR / 'static_files'
+STATIC_ROOT = os.path.join(BASE_DIR,"static_files"),
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
