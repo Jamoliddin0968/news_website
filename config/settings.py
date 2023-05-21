f16c027451d517b3851ffd9da6c48761519827eb
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-05-21 11:48:56 +0500

    ip

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['http://0.0.0.0','https://0.0.0.0']
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
